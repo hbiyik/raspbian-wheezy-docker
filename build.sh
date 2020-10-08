019ad936c1f738bd26c2ae1a521064534da1f997
hbiyik <huseyinbiyik@hotmail.com>
2020-10-08 12:24:37 +0200

    add rootfs

--- build.sh
+++ build.sh
@@ -1,1 +1,6 @@
+wget -N https://raw.githubusercontent.com/hbiyik/raspbian-wheezy-docker/master/rootfs.tar.xz.partaa
+wget -N https://raw.githubusercontent.com/hbiyik/raspbian-wheezy-docker/master/rootfs.tar.xz.partab
+wget -N https://raw.githubusercontent.com/hbiyik/raspbian-wheezy-docker/master/rootfs.tar.xz.partac
+wget -N https://raw.githubusercontent.com/hbiyik/raspbian-wheezy-docker/master/rootfs.tar.xz.partad
+cat rootfs.tar.xz.part* >rootfs.tar.xz
 docker build -t boogiepy/raspbian-wheezy:latest .
